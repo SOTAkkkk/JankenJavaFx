4cd9b90e58c7161098e1348e87127e22a510040c
SOTAkk <88602677+SOTAkk@users.noreply.github.com>
2021-08-18 14:22:29 +0900

    インスタンス化目指す

--- src/main/java/com/example/javafx/visitorPlayer.java
+++ src/main/java/com/example/javafx/visitorPlayer.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class visitorPlayer {
     public static int visitorPlayerHand;
+    public static String ID;
+    public static String PassWord;
+    public static int MaxWin;
 
     static void makeHand(int a){
         visitorPlayerHand = a;
