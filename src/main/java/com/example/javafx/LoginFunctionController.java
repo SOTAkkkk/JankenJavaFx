cacc05beed86fee3d15d985ee6cc38414287ca3e
SOTAkk <88602677+SOTAkk@users.noreply.github.com>
2021-08-18 16:34:34 +0900

    md追加

--- src/main/java/com/example/javafx/LoginFunctionController.java
+++ src/main/java/com/example/javafx/LoginFunctionController.java
@@ -22,11 +22,13 @@ public class LoginFunctionController {
 
     @FXML
     void onButtonLoginAction(ActionEvent event) {
+
         if(IDtext.getText().equals("sota")&&PassWordtext.getText().equals("sota")) {
             status.setText(IDtext.getText() + "さんがログイン！！");
+
         }
         else{
-            status.setText("ログイン失敗");
+            status.setText("IDかPassWordが間違っています");
         }
     }
     @FXML
